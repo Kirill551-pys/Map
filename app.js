657047de14cc84e3451e872217b76546a725ac43
Kirill <taranyuk31@mail.ru>
2024-09-22 17:07:06 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+class ErrorRepository {
+    constructor() {
+      this.errors = new Map([
+        [1, 'Character is already in the team'],
+        [2, 'Invalid character'],
+        // add more error codes and descriptions as needed
+      ]);
+    }
+  
+    translate(code) {
+      return this.errors.get(code) || 'Unknown error';
+    }
+  }
+  
+  export default ErrorRepository;
---
+++ test/test.js
@@ -0,0 +1,22 @@
+import ErrorRepository from './ErrorRepository';
+
+describe('ErrorRepository', () => {
+  let errorRepository;
+
+  beforeEach(() => {
+    errorRepository = new ErrorRepository();
+  });
+
+  it('переводит известный код ошибки', () => {
+    expect(errorRepository.translate(1)).toBe('Character is already in the team');
+  });
+
+  it('возвращает "Неизвестная ошибка" для неизвестного кода ошибки', () => {
+    expect(errorRepository.translate(3)).toBe('Unknown error');
+  });
+
+  it('возвращает "Неизвестную ошибку" для нулевого или неопределенного кода ошибки', () => {
+    expect(errorRepository.translate(null)).toBe('Unknown error');
+    expect(errorRepository.translate(undefined)).toBe('Unknown error');
+  });
+});
